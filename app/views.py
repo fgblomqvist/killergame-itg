3a9b6e98ce4b2153b9b4c8fe65c05bec07cdb4d5
Fredrik Blomqvist <fredrik.blomqvist.95@gmail.com>
2013-09-08 23:48:12 +0200

    Added simple reporting page

--- app/views.py
+++ app/views.py
@@ -1,9 +1,38 @@
 from flask import render_template, request
-from app import app, models
+from app import app, db, models
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return 'ITG Killergame'
+    if request.method == 'POST':
+
+        try:
+            killer_id = int(request.form['killer'])
+            victim_id = int(request.form['victim'])
+        except ValueError:
+            return 'Invalid data!'
+
+        killer = models.Player.query.filter(models.Player.id == killer_id).one()
+        victim = models.Player.query.filter(models.Player.id == victim_id).one()
+
+        # verify that the killer killed the right person
+        if killer.offer_id != victim_id:
+            return "These are not the droids you're looking for"
+
+        # give the killer cred
+        killer.score += 1
+
+        # transfer the victim
+        killer.offer_id = victim.offer_id
+
+        # declare the victim as dead by setting its offer_id to None
+        victim.offer_id = None
+
+        db.session.commit()
+
+        return 'Dadadum dum dum...another bitch bites the dust!'
+
+    else:
+        return render_template('index.html')
 
 
 @app.route('/players')
