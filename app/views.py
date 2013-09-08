96c4635a5a71f5bbb74d1ebbcc8cf7f40b2e7ada
Fredrik Blomqvist <fredrik.blomqvist.95@gmail.com>
2013-09-08 22:19:18 +0200

    Added simple player listing page

--- app/views.py
+++ app/views.py
@@ -1,7 +1,15 @@
-from app import app
+from flask import render_template, request
+from app import app, models
 
 @app.route('/')
 def home():
     return 'ITG Killergame'
 
+
+@app.route('/players')
+def players():
+
+    players = models.Player.query.all()
+
+    return render_template('players.html', players=players)
 
