da6c45f3f1fcd9eee6c57ee41e0d2ed9076e232d
Fredrik Blomqvist <fredrik.blomqvist.95@gmail.com>
2013-09-08 22:09:10 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='killergame-itg',
+    version='0.1',
+    author='Fredrik Blomqvist',
+    author_email='fredrik.blomqvist.95@gmail.com',
+    install_requires=[
+        'flask',
+        'flask-sqlalchemy',
+    ],
+)
