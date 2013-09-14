57c09339c058750e61de2542cbb8b34d64aa92dd
Fredrik Blomqvist <fredrik.blomqvist.95@gmail.com>
2013-09-14 17:37:29 +0200

    Added more templates and styling

---
+++ app/static/js/countdown.js
@@ -0,0 +1,22 @@
+var timelimit = 20;
+
+window.onload = function() {
+    if (window.location.pathname != '/') {
+        var counter = document.createElement('p');
+        counter.className = 'counter'
+        counter.innerText = timelimit.toString();
+        document.body.getElementsByClassName('wrapper')[0].appendChild(counter);
+
+        var i = timelimit;
+
+        setInterval(function() {
+            i -= 1;
+            var counter = document.getElementsByClassName('counter')[0];
+            counter.innerText = i;
+            if (i == 0) {
+                window.location.pathname = '/';
+            }
+
+        }, 1000);
+    }
+};
--- app/views.py
+++ app/views.py
@@ -1,38 +1,31 @@
-from flask import render_template, request
+from flask import render_template, request, url_for, redirect
+from sqlalchemy import orm
 from app import app, db, models
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    if request.method == 'POST':
+    return render_template('index.html')
 
-        try:
-            killer_id = int(request.form['killer'])
-            victim_id = int(request.form['victim'])
-        except ValueError:
-            return 'Invalid data!'
 
-        killer = models.Player.query.filter(models.Player.id == killer_id).one()
-        victim = models.Player.query.filter(models.Player.id == victim_id).one()
+@app.route('/player', methods=['POST'])
+def player():
 
-        # verify that the killer killed the right person
-        if killer.offer_id != victim_id:
-            return "These are not the droids you're looking for"
+    try:
+        player_id = int(request.form['player'])
+    except ValueError:
+        return 'Invalid player ID'
 
-        # give the killer cred
-        killer.score += 1
+    try:
+        player = models.Player.query.filter(models.Player.id == player_id).one()
+    except orm.exc.NoResultFound:
+        return "A player with this ID doesn't exist"
 
-        # transfer the victim
-        killer.offer_id = victim.offer_id
-
-        # declare the victim as dead by setting its offer_id to None
-        victim.offer_id = None
-
-        db.session.commit()
-
-        return 'Dadadum dum dum...another bitch bites the dust!'
-
+    if player.offer_id:
+        target = models.Player.query.filter(models.Player.id == player.offer_id).one()
     else:
-        return render_template('index.html')
+        target = None
+
+    return render_template('player.html', player=player, target=target)
 
 
 @app.route('/players')
@@ -42,3 +35,43 @@ def players():
 
     return render_template('players.html', players=players)
 
+
+@app.route('/confirm', methods=['POST'])
+def confirm():
+
+    try:
+        killer_id = int(request.form['player'])
+        target_id = int(request.form['target'])
+    except ValueError:
+        return 'Invalid data!'
+
+    # if the ids match, go back to home
+    if killer_id == target_id:
+        return redirect(url_for('home'), code=307)
+
+    killer = models.Player.query.filter(models.Player.id == killer_id).one()
+    target = models.Player.query.filter(models.Player.id == target_id).one()
+
+    # if the killer is dead, go back
+    if not killer.offer_id:
+        return redirect(url_for('home'), code=307)
+
+    # verify that the killer killed the right person
+    if killer.offer_id != target_id:
+        return render_template('wrong_target.html', target=target)
+
+    # give the killer cred
+    killer.score += 1
+
+    # transfer the target
+    killer.offer_id = target.offer_id
+
+    # declare the victim as dead by setting its offer_id to None
+    target.offer_id = None
+
+    # fetch the info about the new target
+    target = models.Player.query.filter(models.Player.id == killer.offer_id).one()
+
+    db.session.commit()
+
+    return render_template('target_killed.html', player=killer, target=target)
