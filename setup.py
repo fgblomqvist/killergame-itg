c8d15fcbfbbc4336349b14425201f535ba72f43e
Fredrik Blomqvist <fredrik.blomqvist.95@gmail.com>
2013-09-30 23:11:43 +0200

    Bumped version to 1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='killergame-itg',
-    version='0.1',
+    version='1.0',
     author='Fredrik Blomqvist',
     author_email='fredrik.blomqvist.95@gmail.com',
     install_requires=[
