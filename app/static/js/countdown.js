6884c150f6b7dec361c8f3b0fbde32475e6fd053
Fredrik Blomqvist <fredrik.blomqvist.95@gmail.com>
2013-10-02 22:44:47 +0200

    Replaced innerText with innerHTML
    innerText was apparently some mumbojumbo from IE, so it didn't work in FF

--- app/static/js/countdown.js
+++ app/static/js/countdown.js
@@ -4,7 +4,7 @@ window.onload = function() {
     if (window.location.pathname != '/' && window.location.pathname != '/players' && window.location.pathname != '/gameover') {
         var counter = document.createElement('p');
         counter.className = 'counter'
-        counter.innerText = timelimit.toString();
+        counter.innerHTML = timelimit.toString();
         document.body.getElementsByClassName('wrapper')[0].appendChild(counter);
 
         var i = timelimit;
@@ -12,7 +12,7 @@ window.onload = function() {
         setInterval(function() {
             i -= 1;
             var counter = document.getElementsByClassName('counter')[0];
-            counter.innerText = i;
+            counter.innerHTML = i;
             if (i == 0) {
                 window.location.pathname = '/';
             }
